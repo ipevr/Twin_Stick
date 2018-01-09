b75f1b984f8c2472c590809b93a69f9e193a816e
INGO-PC\Ingo <i.peters@t-online.de>
2018-01-09 21:26:03 +0100

    Rollerball, Camera follows ball (not working yet)

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Ball;
+
+public class CameraController : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.Find ("RollerBall").GetComponent<Ball> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float interpolation = speed * Time.deltaTime;
+		Vector3 position = transform.position;
+		position.x = Mathf.Lerp (transform.position.x, ball.gameObject.transform.position.x, interpolation);
+		position.y = Mathf.Lerp (transform.position.y, ball.gameObject.transform.position.y, interpolation);
+		position.z = Mathf.Lerp (transform.position.z, ball.gameObject.transform.position.z, interpolation);
+		transform.position = position;
+	}
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yAxis = CrossPlatformInputManager.GetAxis ("Vertical");
+		float xAxis = CrossPlatformInputManager.GetAxis ("Horizontal");
+		if (xAxis != 0 || yAxis != 0) {
+			Debug.Log ("H: " + xAxis);
+			Debug.Log ("V: " + yAxis);
+		}
+	}
+}
